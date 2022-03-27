4c96f28dd7dd15c8bbab4c7699d5f47c2b17aed8
Ayush Tiwari <ayushtiwari26402@gmail.com>
2022-03-28 01:02:20 +0530

    added nodemailer route

--- mailService/index.js
+++ mailService/index.js
@@ -1,42 +1,38 @@
-const mailgun = require("mailgun-js");
-
-const DOMAIN = 'mail.portfolioshop.tech';
-const api_key = process.env["MAILGUN_API_KEY"];
+const nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 
 module.exports = async function (context, req) {
-    const data = new URLSearchParams(req.body)
-    const from = data.get("name") + " <noreply@portfolioshop.tech>";
-    const to = data.get("user");
-    const subject = data.get("subject");
-    const text = data.get("message") +'\n Email of the Sender: '+data.get("email");
+  const data = new URLSearchParams(req.body)
+  const from = data.get("name");
+  const to = data.get("user");
+  const subject = data.get("subject");
+  const text = data.get("message") +'\n From: '+data.get("email");
 
-    const nodemailer = require('nodemailer');
-    var smtpTransport = require('nodemailer-smtp-transport');
-    var transporter = nodemailer.createTransport(smtpTransport ({
-        tls: {minVersion: 'TLSv1',rejectUnauthorized: false},
-            host:'smtp.portfolioshop.tech',
-        secureConnection: false,
-        port: 587,
-        auth: {
-            user: "ayush@portfolioshop.tech",
-            pass: "zrFL$pIfY8",
-        }
-        }));
-        var mailOptions = {
-            from: from,
-            to:to,
-            subject:subject,
-            text: text
-        };
-        transporter.sendMail(mailOptions, function(error, info){
-            if(!error){
-                context.log("Email sent successfully")
-                context.res.status(302)
-            }else{
-                context.log("Error in Nodemailer :: "+error.message);
-                context.res.status(500)
-            }
-        });
-    context.res.header('Location', context.req.headers.origin)
-    context.done()
-}
+  var transporter = nodemailer.createTransport(
+    smtpTransport({
+      tls: { minVersion: "TLSv1", rejectUnauthorized: false },
+      host: "smtp.portfolioshop.tech",
+      secureConnection: false,
+      connectionTimeout: 5 * 60 * 1000,
+      port: 587,
+      auth: {
+        user: "ayush@portfolioshop.tech",
+        pass: process.env["NODEMAILER_PASSWORD"]
+      },
+    })
+  );
+  var mailOptions = {
+    from: "ayush@portfolioshop.tech",
+    to: to,
+    subject: subject,
+    text: text,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (err) {
+    context.log(err.message);
+  }
+
+  context.res.header("Location", context.req.headers.origin);
+  context.done();
+};
---
+++ models/schema.js
@@ -0,0 +1,184 @@
+const mongoose = require("mongoose");
+const dataModel = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  templateData: {
+    website: {
+      title: {
+        type: String,
+      },
+      faviconUrl: {
+        type: String,
+      },
+    },
+    name: {
+      type: String,
+    },
+    tagline: [
+      {
+        type: String,
+      },
+    ],
+    socials: {
+      twitter: {
+        type: String,
+      },
+      facebook: {
+        type: String,
+      },
+      linkedin: {
+        type: String,
+      },
+      github: {
+        type: String,
+      },
+      instagram: {
+        type: String,
+      },
+    },
+    about: {
+      heading: {
+          type: String,
+      },
+      dateOfBirth: {
+          type: Date
+      },
+      age: {
+          type: Number,
+          default: 0
+      },
+      city:{
+          type: String,
+      },
+      phone:{
+          type: String,
+      },
+      email:{
+          type: String,
+      },
+      summary:{
+          type: String,
+      }
+    },
+    skills: [{
+        skill: {
+            type: String,
+        },
+        value:{
+            type: Number
+        }
+    }],
+    resume:{
+        summary:{
+            type: String,
+        },
+        education:[{
+            title:{
+                type: String
+            },
+            period:{
+                type: String
+            },
+            institution:{
+                type: String
+            },
+            marks:{
+                type: String
+            }
+        }],
+        experience:[{
+            title:{
+                type: String
+            },
+            period:{
+                type: String
+            },
+            company:{
+                type: String
+            },
+            description:[{
+                type: String
+            }]
+        }],
+        por:[{
+            title:{
+                type: String
+            },
+            period:{
+                type: String
+            },
+            organisation:{
+                type: String
+            },
+            description:[{
+                type: String
+            }]
+        }],
+        awards:[{
+            title:{
+                type: String
+            },
+            period:{
+                type: String
+            },
+            issuer:{
+                type: String
+            },
+            description:[{
+                type: String
+            }]
+        }],
+        publications:[{
+            title:{
+                type: String
+            },
+            gist:{
+                type: String
+            },
+            period:{
+                type: String
+            }
+        }],
+        cocurricular:[{
+            title:{
+                type: String
+            },
+            description:{
+                type: String
+            }
+        }],
+    },
+    projects:[{
+        type:{
+            type: String
+        },
+        imageUrl:{
+            type: String
+        },
+        link:{
+            type: String
+        },
+        title:{
+            type: String
+        },
+        description:{
+            type: String
+        }
+    }],
+    contact:{
+        email:{
+            type: String
+        },
+        phone:{
+            type: String
+        },
+        address:{
+            type: String
+        }
+    }
+  },
+});
+
+module.exports = mongoose.model('UserData', dataModel)
