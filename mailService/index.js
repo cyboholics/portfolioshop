0f3a78e01a6eb931a98983a186b6ff3ec228bfac
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-03-28 02:44:36 +0530

    delete unnecessary libraries

--- mailService/index.js
+++ mailService/index.js
@@ -2,37 +2,37 @@ const nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
 
 module.exports = async function (context, req) {
-  const data = new URLSearchParams(req.body)
-  const from = data.get("name");
-  const to = data.get("user");
-  const email = data.get("email")
-  const subject = data.get("subject");
-  const text = data.get("message") +'\n From: '+data.get("email");
+    const data = new URLSearchParams(req.body)
+    const from = data.get("name");
+    const to = data.get("user");
+    const email = data.get("email")
+    const subject = data.get("subject");
+    const text = data.get("message") + '\n From: ' + data.get("email");
 
-  var transporter = nodemailer.createTransport(
-    smtpTransport({
-      service: 'gmail',
-      host: 'smtp.gmail.com',
-      connectionTimeout: 5 * 60 * 1000,
-      auth: {
-        user: "portfolioshop.tech@gmail.com",
-        pass: process.env["NODEMAILER_PASSWORD"]
-      },
-    })
-  );
-  var mailOptions = {
-    from: `${from} <portfolioshop.tech@gmail.com>`,
-    to: to,
-    subject: subject,
-    text: text,
-  };
-  try {
-    await transporter.sendMail(mailOptions);
-    context.res.status(302)
-  } catch (err) {
-    context.log(err.message);
-    context.res.status(500)
-  }
-  context.res.header("Location", context.req.headers.origin);
-  context.done();
+    var transporter = nodemailer.createTransport(
+        smtpTransport({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            connectionTimeout: 5 * 60 * 1000,
+            auth: {
+                user: "portfolioshop.tech@gmail.com",
+                pass: process.env["NODEMAILER_PASSWORD"]
+            },
+        })
+    );
+    var mailOptions = {
+        from: `${from} <portfolioshop.tech@gmail.com>`,
+        to: to,
+        subject: subject,
+        text: text,
+    };
+    try {
+        await transporter.sendMail(mailOptions);
+        context.res.status(302)
+    } catch (err) {
+        context.log(err.message);
+        context.res.status(500)
+    }
+    context.res.header("Location", context.req.headers.origin);
+    context.done();
 };
