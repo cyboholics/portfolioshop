5d8a64b52092b9818ea51ae34fead8da4f458dc1
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-03-28 12:43:53 +0530

    add user read function

--- models/schema.js
+++ models/schema.js
@@ -1,184 +1,187 @@
 const mongoose = require("mongoose");
+const findOrCreate = require("mongoose-findorcreate")
 const dataModel = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  templateData: {
-    website: {
-      title: {
+    username: {
         type: String,
-      },
-      faviconUrl: {
-        type: String,
-      },
+        required: true,
     },
-    name: {
-      type: String,
-    },
-    tagline: [
-      {
-        type: String,
-      },
-    ],
-    socials: {
-      twitter: {
-        type: String,
-      },
-      facebook: {
-        type: String,
-      },
-      linkedin: {
-        type: String,
-      },
-      github: {
-        type: String,
-      },
-      instagram: {
-        type: String,
-      },
-    },
-    about: {
-      heading: {
-          type: String,
-      },
-      dateOfBirth: {
-          type: Date
-      },
-      age: {
-          type: Number,
-          default: 0
-      },
-      city:{
-          type: String,
-      },
-      phone:{
-          type: String,
-      },
-      email:{
-          type: String,
-      },
-      summary:{
-          type: String,
-      }
-    },
-    skills: [{
-        skill: {
+    templateData: {
+        website: {
+            title: {
+                type: String,
+            },
+            faviconUrl: {
+                type: String,
+            },
+        },
+        name: {
             type: String,
         },
-        value:{
-            type: Number
-        }
-    }],
-    resume:{
-        summary:{
-            type: String,
+        tagline: [
+            {
+                type: String,
+            },
+        ],
+        socials: {
+            twitter: {
+                type: String,
+            },
+            facebook: {
+                type: String,
+            },
+            linkedin: {
+                type: String,
+            },
+            github: {
+                type: String,
+            },
+            instagram: {
+                type: String,
+            },
         },
-        education:[{
-            title:{
+        about: {
+            heading: {
+                type: String,
+            },
+            dateOfBirth: {
+                type: Date
+            },
+            age: {
+                type: Number,
+                default: 0
+            },
+            city: {
+                type: String,
+            },
+            phone: {
+                type: String,
+            },
+            email: {
+                type: String,
+            },
+            summary: {
+                type: String,
+            }
+        },
+        skills: [{
+            skill: {
+                type: String,
+            },
+            value: {
+                type: Number
+            }
+        }],
+        resume: {
+            summary: {
+                type: String,
+            },
+            education: [{
+                title: {
+                    type: String
+                },
+                period: {
+                    type: String
+                },
+                institution: {
+                    type: String
+                },
+                marks: {
+                    type: String
+                }
+            }],
+            experience: [{
+                title: {
+                    type: String
+                },
+                period: {
+                    type: String
+                },
+                company: {
+                    type: String
+                },
+                description: [{
+                    type: String
+                }]
+            }],
+            por: [{
+                title: {
+                    type: String
+                },
+                period: {
+                    type: String
+                },
+                organisation: {
+                    type: String
+                },
+                description: [{
+                    type: String
+                }]
+            }],
+            awards: [{
+                title: {
+                    type: String
+                },
+                period: {
+                    type: String
+                },
+                issuer: {
+                    type: String
+                },
+                description: [{
+                    type: String
+                }]
+            }],
+            publications: [{
+                title: {
+                    type: String
+                },
+                gist: {
+                    type: String
+                },
+                period: {
+                    type: String
+                }
+            }],
+            cocurricular: [{
+                title: {
+                    type: String
+                },
+                description: {
+                    type: String
+                }
+            }],
+        },
+        projects: [{
+            type: {
                 type: String
             },
-            period:{
+            imageUrl: {
                 type: String
             },
-            institution:{
+            link: {
                 type: String
             },
-            marks:{
+            title: {
+                type: String
+            },
+            description: {
                 type: String
             }
         }],
-        experience:[{
-            title:{
+        contact: {
+            email: {
                 type: String
             },
-            period:{
+            phone: {
                 type: String
             },
-            company:{
-                type: String
-            },
-            description:[{
-                type: String
-            }]
-        }],
-        por:[{
-            title:{
-                type: String
-            },
-            period:{
-                type: String
-            },
-            organisation:{
-                type: String
-            },
-            description:[{
-                type: String
-            }]
-        }],
-        awards:[{
-            title:{
-                type: String
-            },
-            period:{
-                type: String
-            },
-            issuer:{
-                type: String
-            },
-            description:[{
-                type: String
-            }]
-        }],
-        publications:[{
-            title:{
-                type: String
-            },
-            gist:{
-                type: String
-            },
-            period:{
+            address: {
                 type: String
             }
-        }],
-        cocurricular:[{
-            title:{
-                type: String
-            },
-            description:{
-                type: String
-            }
-        }],
+        }
     },
-    projects:[{
-        type:{
-            type: String
-        },
-        imageUrl:{
-            type: String
-        },
-        link:{
-            type: String
-        },
-        title:{
-            type: String
-        },
-        description:{
-            type: String
-        }
-    }],
-    contact:{
-        email:{
-            type: String
-        },
-        phone:{
-            type: String
-        },
-        address:{
-            type: String
-        }
-    }
-  },
 });
 
-module.exports = mongoose.model('UserData', dataModel)
+dataModel.plugin(findOrCreate)
+
+module.exports = mongoose.model('Userdata', dataModel)
--- userDb/index.js
+++ userDb/index.js
@@ -1,13 +1,27 @@
+const userData = require("../models/schema")
+const mongoose = require("mongoose")
+const mongoLink = process.env["MONGO_LINK"]
+mongoose.connect(mongoLink, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, result) => {
+    console.log(err || result)
+})
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    var user = req.query["user"]
+    try {
+        var data = await userData.findOrCreate({
+            username: user
+        })
+        context.res = {
+            body: data
+        };
+        context.statusCode = 200;
+    } catch (err) {
+        context.statusCode = 500;
+        context.res = {
+            body: "Internal Server Error"
+        };
+    }
+    context.done();
 }
