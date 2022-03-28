fad4362a25b71fdae1ba746595268b1e1de64f12
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-03-28 13:21:59 +0530

    add user data update function

---
+++ userDataUpdate/index.js
@@ -0,0 +1,30 @@
+const userData = require("../models/schema")
+const mongoose = require("mongoose")
+const mongoLink = process.env["MONGO_LINK"]
+mongoose.connect(mongoLink, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, result) => {
+    console.log(err || result)
+})
+
+module.exports = async function (context, req) {
+    const user = req.body.doc.username;
+    const data = req.body.doc;
+    try {
+        await userData.updateOne({
+            username: user,
+        }, data)
+        context.res = {
+            statusCode:200,
+            body: data
+        };
+    }catch(err){
+        context.res = {
+            statusCode:500,
+            body: "Internal Server Error"
+        };
+        console.log(err)
+    }
+
+}
