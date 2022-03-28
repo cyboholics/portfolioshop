ac4eab881cdc403cab212e124df76aaaebc7c588
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-03-28 18:04:18 +0530

    minor fixes

--- imageBlobUpload/index.js
+++ imageBlobUpload/index.js
@@ -1,26 +1,28 @@
 const AZURE_BLOB_CONNECTION_STRING = process.env["AZURE_BLOB_CONNECTION_STRING"]
 const {BlobServiceClient} = require("@azure/storage-blob")
 const {v4} = require('uuid');
-const dataUriToBuffer = require('data-uri-to-buffer');
 
 module.exports = async function (context, req) {
     const uri = req.body["uri"].split(",")[1];
     var buffer = Buffer.from(uri, 'base64');
     try{ 
         const blobServiceClient = await BlobServiceClient.fromConnectionString(AZURE_BLOB_CONNECTION_STRING);
-        const container = "demoblob";
+        const container = "portfolioshopimages";
         const containerClient = await blobServiceClient.getContainerClient(container);
         const blobName = v4();
         const blockBlobClient = containerClient.getBlockBlobClient(blobName);
         await blockBlobClient.upload(buffer, buffer.length);
         context.res = {
+            statusCode: 200,
             body: {
-                uri: `${process.env["AZURE_BLOB_URL"]}${blobName}`
+                uri: `${process.env["AZURE_BLOB_URL"]}/${container}/${blobName}`
             }
         }
     }catch (e) {
             context.res = {
+                statusCode: 500,
                 body: {
+                    message:"Internal Server Error",
                     error: e.message
                 }
         }
