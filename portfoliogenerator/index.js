7f77435e9299d8b753325045fd4cb8daf66e2883
Ayush Tiwari <ayushtiwari26402@gmail.com>
2022-04-03 14:18:34 +0530

    added template source from blob

--- portfoliogenerator/index.js
+++ portfoliogenerator/index.js
@@ -1,14 +1,50 @@
+const AZURE_BLOB_CONNECTION_STRING = process.env["AZURE_BLOB_CONNECTION_STRING"]
+const { BlobServiceClient } = require("@azure/storage-blob")
 const ejs = require('ejs')
-const path = require("path")
-const fs = require("fs")
-module.exports = async function (context, req) {
-    const data = req.body.data
-    const template = req.body.template
-    const templateFile = fs.readFileSync(__dirname + "/views/"+template+'.ejs', "utf8")
-    context.res.body = ejs.render(templateFile, data)
-    context.res.statusCode = 200
-    context.res.headers = {
-        'Content-Type' : "text/html"
+
+async function streamToText(readable) {
+    readable.setEncoding('utf8');
+    let data = '';
+    for await (const chunk of readable) {
+        data += chunk;
     }
-    context.done()
+    return data;
+}
+
+module.exports = async function (context, req) {
+    const data = req.body["data"]
+    const template = req.body["template"]
+    if (!data || !template) {
+        context.res = {
+            statusCode: 400,
+            body: "Bad Request"
+        }
+    }
+    try {
+        const blobServiceClient = await BlobServiceClient.fromConnectionString(AZURE_BLOB_CONNECTION_STRING)
+        const container = "templates"
+        //gets cintainer
+        const containerClient = await blobServiceClient.getContainerClient(container)
+        //gets template file
+        const blockBlobClient = containerClient.getBlockBlobClient(template + ".ejs");
+        const downloadBlockBlobResponse = await blockBlobClient.download(0);
+        const templateFile = await streamToText(downloadBlockBlobResponse.readableStreamBody)
+        const result = ejs.render(templateFile, data)
+        context.res = {
+            statusCode: 200,
+            body: result,
+            headers: {
+                'Content-Type': "text/html"
+            }
+        }
+        context.done()
+    } catch (err) {
+        context.res = {
+            statusCode: 500,
+            body: err.message
+        }
+    }
+
+
+
 }
