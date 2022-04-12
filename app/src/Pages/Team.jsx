5829c19e88905f18f2cb563a9ab0e35a1d8eff89
Ayush Tiwari <ayushtiwari26402@gmail.com>
2022-04-13 00:27:52 +0530

    wip: team page

--- app/src/Pages/Team.jsx
+++ app/src/Pages/Team.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import TeamCard from '../Components/Team/TeamCard'
+import {Grid, Typography} from '@mui/material'
+
 const Team = () => {
     const leads = [{
         imageLink: "https://avatars.githubusercontent.com/u/74463091?v=4",
@@ -18,9 +20,18 @@ const Team = () => {
     }]
     return (
         <>
-            {leads.map((lead) => {
-                return <TeamCard key={lead.github} imageLink={lead.imageLink} name={lead.name} title={lead.title} description={lead.description} linkedin={lead.linkedin} github={lead.github} />
-            })}
+            <Typography variant="h4" sx={{color: "text.primary", margin: 5}} align="center">Portfolio Shop Team</Typography>
+            <Grid 
+            container
+            spacing={'5%'}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            >                
+                    {leads.map((lead) => {
+                        return <Grid item><TeamCard key={lead.github} imageLink={lead.imageLink} name={lead.name} title={lead.title} description={lead.description} linkedin={lead.linkedin} github={lead.github} /></Grid>
+                    })}
+            </Grid>
         </>
     );
 }
