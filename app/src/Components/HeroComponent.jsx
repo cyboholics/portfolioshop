70c095c370de9f69856a139eaea13c5b6c38e394
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-04-13 15:19:48 +0530

    added theme file

---
+++ app/src/Components/HeroComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Container} from '@mui/material'
+const Herocomponent = () => {
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
+
+export default Herocomponent;
