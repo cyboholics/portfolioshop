fe0d804bd10cdda5f6bc363410f14e7aa6e74361
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-04-14 18:32:00 +0530

    add newsletter frontend

--- app/src/Pages/Landing.jsx
+++ app/src/Pages/Landing.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroComponent from '../Components/HomePage/HeroComponent';
 import Howitworks from '../Components/HomePage/HowitWorks';
+import ProductCTA from '../Components/HomePage/ProductCTA';
 
 const Landing = () => {
     return (
         <>
             <HeroComponent/>
             <Howitworks/>
+            <ProductCTA/>
         </>
     );
 }
