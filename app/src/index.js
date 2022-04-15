9f482119c716f62548bcded1c75f5233a5a5271f
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-04-15 19:50:06 +0530

    remove scrollbar

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,7 @@ import EnvironmentProvider from './Providers/EnvironmentProvider'
 import UserStateProvider from './Providers/UserStateProvider'
 import { BrowserRouter } from "react-router-dom"
 import { createRoot } from 'react-dom/client'
+import './index.css'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
