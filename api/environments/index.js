4d3bd87dd5b8eeb3ffe234ce62ce9e6c1659e90e
Ayush Tiwari <ayushtiwari26402@gmail.com>
2022-04-15 14:11:31 +0530

    minor changes

--- api/environments/index.js
+++ api/environments/index.js
@@ -1,8 +1,18 @@
 module.exports = async function (context, req) {
-    context.res = {
-        body: {
-            "GOOGLE_CLIENT_ID": process.env["REACT_APP_GOOGLE_AUTH_CLIENT_ID"]
+    const REACT_APP_GOOGLE_AUTH_CLIENT_ID = process.env["REACT_APP_GOOGLE_AUTH_CLIENT_ID"]
+    if (!REACT_APP_GOOGLE_AUTH_CLIENT_ID) {
+        context.res = {
+            statusCode: 500,
+            body: "Environment Not Found"
         }
+        context.done()
+    } else {
+        context.res = {
+            statusCode: 200,
+            body: {
+                "GOOGLE_CLIENT_ID": process.env["REACT_APP_GOOGLE_AUTH_CLIENT_ID"]
+            }
+        }
+        context.done()
     }
-    context.done()
 }
