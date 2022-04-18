874cbd39a3629941a17dbc36b8c891e9a65d5aee
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-04-18 16:41:50 +0530

    refactored upload button

--- app/src/Components/ThirdPartyButtons/DonateButton.jsx
+++ app/src/Components/ThirdPartyButtons/DonateButton.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
 const DonateButton = ({ buttonId }) => {
+    const [id,] = React.useState(buttonId)
     React.useEffect(() => {
-        const donateForm = document.getElementById(buttonId)
+        const donateForm = document.getElementById(id)
         const formScript = document.createElement("script");
         formScript.setAttribute('src', "https://checkout.razorpay.com/v1/payment-button.js")
         formScript.setAttribute('data-payment_button_id', 'pl_JKPvIiQu72kFIv')
         donateForm.appendChild(formScript)
-    }, [buttonId])
+    }, [id])
     return (
-        <form id={buttonId}></form>
+        <form id={id}></form>
     )
 }
 
