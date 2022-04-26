363cd51e8d71ba365c253675518ad2b39f8f3d33
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-04-26 18:02:12 +0530

    fix your website button

--- app/src/Pages/Dashboard.jsx
+++ app/src/Pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box, Container, Button } from '@mui/material'
 import GoogleLogoutComp from '../Components/ThirdPartyButtons/GoogleLogoutComp'
 import UserDataStateProvider from '../Providers/UserDataStateProvider'
-import UserStateProvider from '../Providers/UserStateProvider'
+import { UserContext } from '../Providers/UserStateProvider'
 import Website from '../Components/Dashboard/Website'
 import UserDisplayItems from '../Components/Dashboard/UserDisplayItems'
 import About from '../Components/Dashboard/About'
@@ -11,19 +11,18 @@ import Resume from '../Components/Dashboard/Resume'
 import Projects from '../Components/Dashboard/Projects'
 import Contact from '../Components/Dashboard/Contact'
 import Savebutton from '../Components/Dashboard/SaveButton'
-import {Link} from 'react-router-dom'
 const Dashboard = () => {
-    const {userEmail} = React.useContext(UserStateProvider)
+    const { userEmail } = React.useContext(UserContext)
     return (
         <>
             <Box sx={{ marginTop: 10 }}>
                 <UserDataStateProvider>
                     <Container>
-                        <Link to={`/api/portfolioGenerator?email=${userEmail}`}>
-                        <Button color="info" variant="outlined">
-                            Visit your website
-                        </Button>
-                        </Link>
+                        <a href={`/api/portfolioGenerator?email=${userEmail}`} target="blank" style={{textDecoration: "none"}}>
+                            <Button color="info" variant="outlined">
+                                Visit your website
+                            </Button>
+                        </a>
                         <GoogleLogoutComp />
                         <Website />
                         <UserDisplayItems />
