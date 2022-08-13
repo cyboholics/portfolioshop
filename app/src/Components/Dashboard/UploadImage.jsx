25f8240cb393c9471ce28d13fc6c2cd99482599b
Joiyad <20bee012@nith.ac.in>
2022-08-13 12:20:07 +0530

    SSOC(#67)

--- app/src/Components/Dashboard/UploadImage.jsx
+++ app/src/Components/Dashboard/UploadImage.jsx
@@ -5,8 +5,18 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload'
 import axios from 'axios'
 import { UserContext } from '../../Providers/UserStateProvider'
 import ReportGmailerrorredOutlinedIcon from '@mui/icons-material/ReportGmailerrorredOutlined';
+import { Box } from '@mui/system'
 
 export default function UploadImage({ imageLink, onChange, width, height, circular }) {
+    // For testing on local server
+    // const [images, setImages] = React.useState([]);
+    // const onChanged = (imageList, addUpdateIndex) => {
+    //     console.log(imageList, addUpdateIndex);
+    //     setImages(imageList);
+    // };
+
+
+
     const { userToken } = React.useContext(UserContext)
     const [loading, setLoading] = React.useState(false)
     const [imageError, setImageError] = React.useState(false)
@@ -33,54 +43,106 @@ export default function UploadImage({ imageLink, onChange, width, height, circul
     return (
         <div className="App">
             <ImageUploading
+                imageList
                 multiple={false}
-                value={imageLink}
-                onChange={upload}
+                value={imageLink} //use value={images}
+                onChange={upload} //use onChange={onChanged}
                 dataURLKey="data_url"
             >
                 {({
+                    imageList,
                     onImageUpload,
+                    onImageUpdate,
+                    onImageRemove,
                     isDragging,
                     dragProps,
                 }) => (
                     // write your building UI
-                    <Stack sx={{
-                        marginTop: 1,
-                    }}
-                    direction={"row"}
-                    spacing={2}
-                    >
-                        <Button
-                            variant="outlined"
-                            color={isDragging ? 'info' : 'warning'}
+                    <Box
+                        sx={{
+                            width:"230px",
+                            padding: 4,
+                            marginX:"auto",
+                            marginY:20,
+                            boxShadow: 1,
+                            borderRadius:"16px",
+                            alignItems:"center"
+                        }}
+                        >
+                        <Typography variant="body1" align="center" marginBottom={2} sx={{fontWeight:500}}>
+                            Upload Image
+                        </Typography>
+                        <Stack 
+                        justifyContent="center"
+                        direction={"column"}
+                        spacing={2}
+                        >
+                            <Button
+                                variant="outlined"
+                                color={isDragging ? 'info' : 'warning'}
+                                sx={{
+                                    padding:10,
+                                    border:"1px dashed gray",
+                                    borderRadius:"16px"
+                            }}
                             onClick={onImageUpload}
                             {...dragProps}
-                        >
-                            <CloudUploadIcon />
-                            <Typography variant="body" marginLeft={1}>
-                                Upload
-                            </Typography>
-                        </Button>
-                        {loading ? <CircularProgress 
-                            size={Math.min(40,width,height)}
-                        />: imageError? <ReportGmailerrorredOutlinedIcon
-                            color="error"
+                            >
+                                <CloudUploadIcon />
+                                <Typography variant="body1" marginLeft={1}>
+                                    Upload
+                                </Typography>
+                            </Button>
+                            {loading ? <CircularProgress 
+                                size={Math.min(40,width,height)}
+                                />: imageError? <ReportGmailerrorredOutlinedIcon
+                                color="error"
+                                sx={{
+                                    height: 30,
+                                    width: 30
+                                }}
+                            /> : 
+                        // Earlier it was
+
+                        // imageLink && <img
+                        //     src={imageLink}
+                        //     alt="ico"
+                        //     style={{
+                        //         borderRadius: circular ? "50%" : 0
+                        //     }}
+                        //     onError={()=>{
+                        //         setImageError(true)
+                        //     }}
+                        //     width={width}
+                        //     height={height} />}
+
+                            // Updatad code below
+                            <>
+                            {imageList.map((image, index) => (
+                            <Box marginTop={2} key={index}
                             sx={{
-                                height: 30,
-                                width: 30
-                            }}
-                        /> : imageLink && <img
-                            src={imageLink}
-                            alt="ico"
-                            style={{
-                                borderRadius: circular ? "50%" : 0
-                            }}
-                            onError={()=>{
-                                setImageError(true)
-                            }}
-                            width={width}
-                            height={height} />}
-                    </Stack>
+                                display:"flex",
+                                flexDirection:"row",
+                                justifyContent:"center",
+                                paddingY:"2px",
+                                gap:"8px",
+                                borderRadius:"4px"
+                            }}>
+                                <img 
+                                src={imageLink} //use src={image['data_url]}
+                                alt="img" 
+                                width="150px" height="100px"
+                                />
+                                <Box sx={{display:"flex", flexDirection:"column", justifyContent:"center", gap:"60px"}}>
+                                    <Button variant="outlined" style={{maxWidth:"10px", maxHeight:"20px", fontSize:"10px"}} onClick={() => onImageUpdate(index)}>Update</Button>
+                                    <Button variant="outlined" style={{maxWidth:"10px", maxHeight:"20px", fontSize:"10px"}} onClick={() => onImageRemove(index)}>Remove</Button>
+                                </Box>
+                            </Box>
+                            ))}
+                            </>
+                            }
+                        </Stack>
+                    </Box>
                 )}
             </ImageUploading>
         </div>
