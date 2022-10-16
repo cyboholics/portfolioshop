2b17a9e63f38a480dd27518ec12d7b4111fefe41
pollygen <89333614+pollygen@users.noreply.github.com>
2022-10-16 18:13:45 +0530

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   "ci": {
     "collect": {
       "url": [
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:4280"
       ],
       "startServerCommand": "npm run start-swa",
       "startServerReadyPattern": "webpack compiled with",
